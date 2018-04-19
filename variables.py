c3c169f38fa74f9d6af647c73a3c581b46bfdd88
Unknown <moumtazhakim@yahoo.fr>
2018-04-19 16:59:55 +0200

    Supression

--- variables.py
+++ variables.py
@@ -19,7 +19,7 @@
 #
 #   Définir la bonne variable pour faire fonctionner le code ci-dessous.
 #
-#   Savoirs-faires :gqsdhsg
+#   Savoirs-faires :
 #       créer une variable et lui affecter une valeur
 
 # ... Écrivez votre code ici ...
